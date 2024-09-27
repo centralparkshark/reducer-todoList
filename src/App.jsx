160da4c85edf52471d76d013632e07274a480542
Vinncent Miller <vinncentmiller@gmail.com>
2024-09-27 16:29:37 -0400

    Added test data

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import './App.css'
 import Item from './components/item'
 import NewItem from './components/newItem'
+import { initialState } from './data/data'
 
 function App() {
 
   return (
     <div className="main">
       <h1>To Do List</h1>
-      <Item task="Finish assignment"/>
-      <Item task="Commit to gitHub"/>
-      <Item task="Check"/>
-      <Item task="Lofsdfjksdhfsjkfjshke fshdfkjsdfhsdfkl"/>
+      {initialState.map((item, index) => <Item key={index} {...item} />)}
       <NewItem/>
     </div>
   )
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react"
+
 export default function Item(props) {
+    const [completed, setCompleted] = useState(props.completed)
+
     return (
-        <div className="item">
-            <input type="checkbox" name="task" id="" />
-            <div className="title">{props.task}</div>
+        <div className="item" key={props.key}>
+            <input type="checkbox" name="task" id="" checked={completed}/>
+            <div className="title">{props.title}</div>
             <button className=" fa fa-edit"></button>
-            <button className=" fa fa-trash" disabled></button>
+            <button className=" fa fa-trash" ></button>
         </div>
     )
 }
